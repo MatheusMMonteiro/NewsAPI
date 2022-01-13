dffd02fd4c21b8bbb9b1f6c8e188edfebdd3e83f
Matheus <matheus.malmeida7@gmail.com>
2022-01-13 19:09:50 -0300

    Adding annotations @RestController

--- src/main/java/com/dynaccurate/newsAPI/NewsApiApplication.java
+++ src/main/java/com/dynaccurate/newsAPI/NewsApiApplication.java
@@ -2,8 +2,10 @@ package com.dynaccurate.newsAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class NewsApiApplication {
 
 	public static void main(String[] args) {
