025db1fa2c0c0f348bd8332204a206efc19aea6f
Matheus <matheus.malmeida7@gmail.com>
2022-01-17 17:45:41 -0300

    Create findByNewsId

--- src/main/java/com/dynaccurate/newsAPI/repository/CommentsRepository.java
+++ src/main/java/com/dynaccurate/newsAPI/repository/CommentsRepository.java
@@ -1,9 +1,12 @@
 package com.dynaccurate.newsAPI.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.dynaccurate.newsAPI.model.Comments;
 
 public interface CommentsRepository extends JpaRepository<Comments, Long>  {
 
+	public List<Comments> findByNewsId(long id);
 }
