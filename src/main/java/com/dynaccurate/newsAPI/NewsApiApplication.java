835a0a0e78a21d607586c3b3f15941619a7bd499
Matheus <matheus.malmeida7@gmail.com>
2022-01-17 21:48:41 -0300

    Adding config of route Swagger

--- src/main/java/com/dynaccurate/newsAPI/NewsApiApplication.java
+++ src/main/java/com/dynaccurate/newsAPI/NewsApiApplication.java
@@ -2,11 +2,20 @@ package com.dynaccurate.newsAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("/")
 public class NewsApiApplication {
+	
+	@GetMapping
+	public ModelAndView swaggerUi() {
+		return new ModelAndView("redirect:/swagger-ui.html");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(NewsApiApplication.class, args);
